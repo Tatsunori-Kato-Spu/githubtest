3daf8acb4fe36e431035bcbc4f82ec9ebcb8ec91
TatsunoriSPU <tatsunori.kat@spumail.net>
2025-02-27 02:31:30 +0700

    react_vite

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React! 🚀</h1>
+      <p>ยินดีต้อนรับสู่ React ด้วย Vite</p>
+    </div>
+  );
+}
+
+export default App;
